1cb5db5581d537a17e45256f6bf9f7d86858dfc7
Omil Dudhat <98299264+ynova019@users.noreply.github.com>
2022-04-23 18:59:15 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_DgAuUOy7U', // e.g. us-east-1_DgAuUOy7U
+        userPoolClientId: '2cfa83764r7lib8td61olbst91', // e.g. 2cfa83764r7lib8td61olbst91
+        region: 'us-east-1' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
